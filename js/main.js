14acdfbb37e81baeb40cb09bd880f713d06406e2
github raamenzurururu <reku9696@gmail.com>
2020-11-20 06:11:47 +0900

    [add]1~6までのランダムな整数表示

--- js/main.js
+++ js/main.js
@@ -1,14 +1,6 @@
 "use strict";
 
 {
-  const prices = [80, 90, 40];
-
-  const updatePrices = prices.map((price) => {
-    return price + 20;
-  })
-
-  // const prices = [80, 90, 40];
-
-  // const updatePrices = prices.map(price =>  price + 20);
-  console.log(updatePrices);
+  // console.log(Math.random());
+  console.log(Math.floor(Math.random() * 6) + 1);
 }
