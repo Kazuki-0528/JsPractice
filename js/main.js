ee79605e1bbe090b3efc345d5b2d9c0e6cb25b0b
github raamenzurururu <reku9696@gmail.com>
2020-11-21 07:40:44 +0900

    [add]DOM操作

--- js/main.js
+++ js/main.js
@@ -1,39 +1,9 @@
 "use strict";
 
 {
-  class Post {
-    constructor(text) {
-      this.text = text;
-      this.likeCount = 0;
-    }
-
-    show() {
-      console.log(`${this.text} - ${this.likeCount} likes`);
-    }
-
-    like() {
-      this.likeCount++;
-      this.show();
-    }
+  function update() {
+    document.getElementById('target').textContent = 'Changed!!!!!!';
   }
 
-  class SponsoredPost extends Post {
-    constructor(text, sponsor) {
-      super(text);
-      this.sponsor = sponsor;
-    }
-
-    show() {
-      super.show()
-      console.log(`... sponsored by ${this.sponsor}`);
-    }
-  }
-
-  const posts = [
-    new Post('JavaScript勉強中'),
-    new Post('English'),
-    new SponsoredPost('3分でマスター', 'install'),
-  ];
-
-  posts[2].show();
+  setTimeout(update, 1000);
 }
