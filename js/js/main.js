b20777aa7caf53eaf6cfcf42cf1d52940c3d4047
github raamenzurururu <reku9696@gmail.com>
2020-11-22 15:56:10 +0900

    [add]勉強時間を伸ばすボタン完成

--- js/js/main.js
+++ js/js/main.js
@@ -4,16 +4,18 @@
   const btn = document.getElementById('btn');
 　
   btn.addEventListener('click', () => {
-    // const results = ['大吉', '中吉', '凶', '末吉'];
-    // const results = ['大吉', '大吉', '凶', '大吉'];
-    // btn.textContent = results[Math.floor(Math.random() * results.length)];
-    const n = Math.random();
-    if (n < 0.05) {
-      btn.textContent = '大吉';
-    } else if (n < 0.2) {
-      btn.textContent = '中吉';
-    } else {
-      btn.textContent = '凶';
+    const n = Math.floor(Math.random() * 3);
+
+    switch (n) {
+      case 0:
+        btn.textContent = '休憩';
+        break;
+      case 1:
+        btn.textContent = 'もう10分';
+        break;
+      case 2:
+        btn.textContent = 'もう15分';
+        break;
     }
   })
 }
